611f9557638907dab5817b954104ea603f592c1e
Judah Rand <17158624+judahrand@users.noreply.github.com>
2024-11-04 22:49:05 +0000

    Remove subcleaner for now

--- kubernetes/main/apps/downloads/bazarr/app/resources/subcleaner.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-printf "Cleaning subtitles for '%s' ...\n" "$1"
-python3 /add-ons/subcleaner/subcleaner.py "$1" -s
-
-case $1 in
-    *movies*) section="1";;
-    *shows*) section="2";;
-esac
