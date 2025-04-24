c97b0d2ad5591203ea7c79c1fe42a275c9af3777
Judah Rand <17158624+judahrand@users.noreply.github.com>
2025-04-24 20:25:21 +0100

    Update bazarr

---
+++ kubernetes/apps/downloads/bazarr/app/resources/subcleaner.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+printf "Cleaning subtitles for '%s' ...\n" "$1"
+python3 /subcleaner/subcleaner/subcleaner.py "$1" -s
+
+case $1 in
+    *movies*) section="1";;
+    *shows*) section="2";;
+esac
+
+if [[ -n "$section" ]]; then
+    printf "Refreshing Jellyfin...\n"
+    /usr/bin/curl -I -X GET -G \
+        -H "Authorization: MediaBrowser Token=${JELLYFIN_TOKEN}" \
+        --no-progress-meter \
+            "http://jellyfin.media.svc.cluster.local:8096/Library/Refresh"
+fi
